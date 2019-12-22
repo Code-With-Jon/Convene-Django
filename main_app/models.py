0cd7c0f106955990426b16a7920d12decefa2cbe
Jon <jon@Sciences-MacBook-Pro.local>
2019-12-21 17:21:25 -0800

    backtracked

--- main_app/models.py
+++ main_app/models.py
@@ -18,9 +18,6 @@ CATEGORIES = (
     ('health', 'Health'),
 )
 
-
-
-
 class Event(models.Model):
     title = models.CharField(max_length=250)
     date = models.DateField('event date')
@@ -29,10 +26,12 @@ class Event(models.Model):
     description = models.TextField(max_length=2000)
     attendees = ArrayField(models.CharField(max_length=250))
     infolink = models.CharField(max_length=1000)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.CharField(
         max_length=100,
         choices=CATEGORIES,
-        default=""
+
     )
 
     users = models.ManyToManyField(User)
@@ -40,14 +39,9 @@ class Event(models.Model):
 
 
     def get_absolute_url(self):
-        return reverse('upload_photo', kwargs={'event_id': self.id})
+        return reverse('events_detail', kwargs={'event_id': self.id})
 
-class Photo(models.Model):
-    url = models.CharField(max_length=200)
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"Photo for event_id: {self.event_id} @{self.url}"
+    
 
 
 class Comment(models.Model):
