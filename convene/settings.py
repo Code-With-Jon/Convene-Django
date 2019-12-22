e9d0920ce15845f245f9b8c6c398fdb978c06725
Jon <jon@Sciences-MacBook-Pro.local>
2019-12-21 17:37:32 -0800

    testing

--- convene/settings.py
+++ convene/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'convene.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'convenedb',
+        'NAME': 'convenedb2',
     }
 }
 
